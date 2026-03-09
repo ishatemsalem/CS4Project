3042cf6c420bc8d11e2ffdad27ce3b025a0f61c1
judyibraheem <Judy.mmhmd@gmail.com>
2026-03-09 20:54:00 +0200

    csv files

--- src/game/engine/dataloader/DataLoader.java
+++ src/game/engine/dataloader/DataLoader.java
@@ -1,5 +1,150 @@
 package game.engine.dataloader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import game.engine.cards.ConfusionCard;
+import game.engine.cards.EnergyStealCard;
+import game.engine.cards.ShieldCard;
+import game.engine.cards.StartOverCard;
+import game.engine.cards.SwapperCard;
+import game.engine.cells.Cell;
+import game.engine.cells.ContaminationSock;
+import game.engine.cells.ConveyorBelt;
+import game.engine.cells.DoorCell;
+import game.engine.monsters.Dasher;
+import game.engine.monsters.Dynamo;
+import game.engine.monsters.Monster;
+import game.engine.monsters.MultiTasker;
+import game.engine.monsters.Schemer;
+
 public class DataLoader {
+    private static final String CARDS_FILE_NAME = "cards.csv";
+    private static final String CELLS_FILE_NAME = "cells.csv";
+    private static final String MONSTERS_FILE_NAME = "monsters.csv";
+
+     public static ArrayList<Card> readCards() throws IOException{
+        ArrayList<Card> cards = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(CARDS_FILE_NAME));
+        String line;
+
+        while((line = br.readLine()) != null){
+            String[] data = line.split(",");
+
+            String type = data[0];
+            String name = data[1];
+            String description = data[2];
+            int rarity = Integer.parseInt(data[3]);
+
+            switch(type){
+
+            case "SwapperCard":
+                cards.add(new SwapperCard(name, description, rarity));
+                break;
+
+            case "ShieldCard":
+                cards.add(new ShieldCard(name, description, rarity));
+                break;
+
+            case "EnergyStealCard":
+                int energy = Integer.parseInt(data[4]);
+                cards.add(new EnergyStealCard(name, description, rarity, energy));
+                break;
+
+            case "StartOverCard":
+                boolean lucky = Boolean.parseBoolean(data[4]);
+                cards.add(new StartOverCard(name, description, rarity, lucky));
+                break;
+
+            case "ConfusionCard":
+                int duration = Integer.parseInt(data[4]);
+                cards.add(new ConfusionCard(name, description, rarity, duration));
+                break;
+            }
+            
+        }
+        br.close(); 
+        return cards;
+     }
+
+    public static ArrayList<Cell> readCells() throws IOException {
+
+        ArrayList<Cell> cells = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(CELLS_FILE_NAME));
+
+        String line;
+
+        while((line = br.readLine()) != null){
+
+            String[] data = line.split(",");
+
+            String name = data[0];
+
+            if(data.length == 3){
+
+                Role role = Role.valueOf(data[1]);
+                int energy = Integer.parseInt(data[2]);
+
+                cells.add(new DoorCell(name, role, energy));
+            }
+
+            else if(data.length == 2){
+
+                String effect = data[1];
+
+                if(effect.equalsIgnoreCase("positive"))
+                    cells.add(new ConveyorBelt(name));
+
+                else
+                    cells.add(new ContaminationSock(name));
+            }
+        }
+
+        br.close();
+        return cells;
+    }
+
+    public static ArrayList<Monster> readMonsters() throws IOException {
+
+        ArrayList<Monster> monsters = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(MONSTERS_FILE_NAME));
+
+        String line;
+
+        while((line = br.readLine()) != null){
+
+            String[] data = line.split(",");
+
+            String type = data[0];
+            String name = data[1];
+            String description = data[2];
+            Role role = Role.valueOf(data[3]);
+            int energy = Integer.parseInt(data[4]);
+
+            switch(type){
+
+                case "Dasher":
+                    monsters.add(new Dasher(name, description, role, energy));
+                    break;
+
+                case "Dynamo":
+                    monsters.add(new Dynamo(name, description, role, energy));
+                    break;
+
+                case "MultiTasker":
+                    monsters.add(new MultiTasker(name, description, role, energy));
+                    break;
+                case "Schemer":
+                    monsters.add(new Schemer(name, description, role, energy));
+                    break;
+            }
+        }
+        
+
+        br.close();
+        return monsters;
+    }
 
 }
