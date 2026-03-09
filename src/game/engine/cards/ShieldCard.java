f2f66ae10730b7abd60cd9df56b56db1eead3545
ishatemsalem <ishatemsalem@gmail.com>
2026-03-09 18:44:06 +0200

    hijacking ur branch (sory)

--- src/game/engine/cards/ShieldCard.java
+++ src/game/engine/cards/ShieldCard.java
@@ -1,5 +1,9 @@
 package game.engine.cards;
 
-public class ShieldCard {
+public class ShieldCard extends Card {
+
+	public ShieldCard(String name, String description, int rarity) {
+		super(name, description, rarity, true);
+	}
 
 }
--- src/game/engine/exceptions/OutOfEnergyException.java
+++ src/game/engine/exceptions/OutOfEnergyException.java
@@ -1,5 +1,14 @@
 package game.engine.exceptions;
 
-public class OutOfEnergyException {
+public class OutOfEnergyException extends GameActionException{
+	public static final String MSG = "Not Enough Energy for Power Up";
+	
+	public OutOfEnergyException() {
+        super(MSG);
+    }
+	public OutOfEnergyException(String message) {
+        super(message);
+    }
 
 }
+
